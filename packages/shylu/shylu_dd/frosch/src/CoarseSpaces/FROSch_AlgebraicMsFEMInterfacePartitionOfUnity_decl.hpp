dc2138c449abf3e82a7c38a7ebd6be582f938cf3
Filipe-Cumaru <filipe.cumaru@ufpe.br>
2024-09-24 13:56:53 +0200

    feat(shylu/frosch): add the boilerplate for the algebraic MsFEM IPOU

---
+++ packages/shylu/shylu_dd/frosch/src/CoarseSpaces/FROSch_AlgebraicMsFEMInterfacePartitionOfUnity_decl.hpp
@@ -0,0 +1,57 @@
+#ifndef _FROSCH_ALGEBRAICMSFEMINTERFACEPARTITIONOFUNITY_DECL_HPP
+#define _FROSCH_ALGEBRAICMSFEMINTERFACEPARTITIONOFUNITY_DECL_HPP
+
+#include <FROSch_GDSWInterfacePartitionOfUnity_def.hpp>
+#include <FROSch_SolverFactory_def.hpp>
+
+namespace FROSch
+{
+    template <class SC = double,
+              class LO = int,
+              class GO = DefaultGlobalOrdinal,
+              class NO = Tpetra::KokkosClassic::DefaultNode::DefaultNodeType>
+    class AlgebraicMsFEMInterfacePartitionOfUnity : public GDSWInterfacePartitionOfUnity<SC, LO, GO, NO>
+    {
+    protected:
+        using UN = typename PartitionOfUnity<SC, LO, GO, NO>::UN;
+
+        using CommPtr = typename PartitionOfUnity<SC, LO, GO, NO>::CommPtr;
+
+        using XMapPtr = typename PartitionOfUnity<SC, LO, GO, NO>::XMapPtr;
+        using ConstXMapPtr = typename PartitionOfUnity<SC, LO, GO, NO>::ConstXMapPtr;
+        using ConstXMapPtrVecPtr = typename PartitionOfUnity<SC, LO, GO, NO>::ConstXMapPtrVecPtr;
+
+        using ParameterListPtr = typename PartitionOfUnity<SC, LO, GO, NO>::ParameterListPtr;
+
+        using XMultiVector = typename PartitionOfUnity<SC, LO, GO, NO>::XMultiVector;
+        using ConstXMultiVectorPtr = typename PartitionOfUnity<SC, LO, GO, NO>::ConstXMultiVectorPtr;
+        using ConstXMultiVectorPtrVecPtr = typename PartitionOfUnity<SC, LO, GO, NO>::ConstXMultiVectorPtrVecPtr;
+
+        using XMatrixPtr = typename PartitionOfUnity<SC, LO, GO, NO>::XMatrixPtr;
+        using ConstXMatrixPtr = typename PartitionOfUnity<SC, LO, GO, NO>::ConstXMatrixPtr;
+
+        using EntitySetPtr = typename PartitionOfUnity<SC, LO, GO, NO>::EntitySetPtr;
+        using EntitySetConstPtr = const EntitySetPtr;
+
+        using SolverPtr = RCP<Solver<SC, LO, GO, NO>>;
+
+    public:
+        AlgebraicMsFEMInterfacePartitionOfUnity(CommPtr mpiComm,
+                                                CommPtr serialComm,
+                                                UN dimension,
+                                                UN dofsPerNode,
+                                                ConstXMapPtr nodesMap,
+                                                ConstXMapPtrVecPtr dofsMaps,
+                                                ParameterListPtr parameterList,
+                                                ConstXMatrixPtr K,
+                                                Verbosity verbosity = All,
+                                                UN levelID = 1);
+
+        virtual int computePartitionOfUnity(ConstXMultiVectorPtr nodeList = null);
+
+    protected:
+        ConstXMatrixPtr K_;
+    };
+}
+
+#endif
---
+++ packages/shylu/shylu_dd/frosch/src/CoarseSpaces/FROSch_AlgebraicMsFEMInterfacePartitionOfUnity_def.hpp
@@ -0,0 +1,38 @@
+#ifndef _FROSCH_ALGEBRAICMSFEMINTERFACEPARTITIONOFUNITY_DEF_HPP
+#define _FROSCH_ALGEBRAICMSFEMINTERFACEPARTITIONOFUNITY_DEF_HPP
+
+#include <FROSch_AlgebraicMsFEMInterfacePartitionOfUnity_decl.hpp>
+
+namespace FROSch
+{
+    template <class SC, class LO, class GO, class NO>
+    AlgebraicMsFEMInterfacePartitionOfUnity<SC, LO, GO, NO>::AlgebraicMsFEMInterfacePartitionOfUnity(CommPtr mpiComm,
+                                                                                                     CommPtr serialComm,
+                                                                                                     UN dimension,
+                                                                                                     UN dofsPerNode,
+                                                                                                     ConstXMapPtr nodesMap,
+                                                                                                     ConstXMapPtrVecPtr dofsMaps,
+                                                                                                     ParameterListPtr parameterList,
+                                                                                                     ConstXMatrixPtr K,
+                                                                                                     Verbosity verbosity,
+                                                                                                     UN levelID) : GDSWInterfacePartitionOfUnity<SC, LO, GO, NO>(mpiComm,
+                                                                                                                                                                 serialComm,
+                                                                                                                                                                 dimension,
+                                                                                                                                                                 dofsPerNode,
+                                                                                                                                                                 nodesMap,
+                                                                                                                                                                 dofsMaps,
+                                                                                                                                                                 parameterList,
+                                                                                                                                                                 verbosity,
+                                                                                                                                                                 levelID),
+                                                                                                                   K_(K)
+    {
+    }
+
+    template <class SC, class LO, class GO, class NO>
+    int AlgebraicMsFEMInterfacePartitionOfUnity<SC, LO, GO, NO>::computePartitionOfUnity(ConstXMultiVectorPtr nodeList)
+    {
+        return GDSWInterfacePartitionOfUnity<SC, LO, GO, NO>::computePartitionOfUnity(nodeList);
+    }
+}
+
+#endif
